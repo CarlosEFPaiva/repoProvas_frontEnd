24970dc5604fcba42a4ba3a131cb5431cd749e53
CarlosEFPaiva <carlosedufpaiva@poli.ufrj.br>
2021-12-14 13:06:18 -0300

    start: Created and installed files for start of new project

---
+++ src/assets/css/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Mukta', sans-serif;
+        font-weight: 400;
+    }
+`;
+
+export default GlobalStyles;
