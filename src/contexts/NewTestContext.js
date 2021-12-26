326a7c2ef587a1ad8f8be9825b61d12c235be7e9
Sidney Outeiro <sid@dcc.ufrj.br>
2021-12-26 01:30:54 -0300

    preparando para o review

--- src/contexts/NewTestContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const NewTestContext = createContext();
-
-export default NewTestContext;
--- src/utils/externalLibs/reactLoader.js
+++
@@ -1,9 +0,0 @@
-import Loader from 'react-loader-spinner';
-
-function Loading({ type, color, height, width }) {
-    return <Loader type={type} color={color} height={height} width={width} />;
-}
-
-export {
-    Loading,
-};
